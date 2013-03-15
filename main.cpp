2248de33f042a236629852301048516c467654bb
mikhaylova-daria <dasham94@yandex.ru>
2013-03-15 20:01:06 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#ifndef STDLIB_H
+#include <stdlib.h>
+#endif
+#ifndef VECTOR_H
+#include "Vector.h"
+#endif
+using namespace std;
+int main()
+{
+	BigNum a;
+	a.input();
+	a.output();
+	return 0;
+}
