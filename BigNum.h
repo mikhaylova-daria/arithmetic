ef208cdce31892154acc81a91f8308dcc3c05abe
mikhaylova-daria <dasham94@yandex.ru>
2013-03-30 02:47:46 +0400

    Update BigNum.h
    ввод - вывод в файл

--- BigNum.h
+++ BigNum.h
@@ -138,6 +138,129 @@ using namespace std;
         free(buf);
         return;
     }
+    
+        void BigNum::finput (FILE *fp) {
+        short* buf = (short*) malloc (sizeof(short) * MAX_SIZE);
+        int cap_buf = MAX_SIZE;
+        int i=0;
+        char c;
+        fscanf(fp, "%c", &c);
+        if (c == '-'){
+            this->sign = false;
+            fscanf(fp, "%c", &c);
+        } else {
+            this -> sign = true;
+        }
+        while (c == '0'){    // пропускает незначащие нули, введённые пользователем
+              fscanf(fp,"%c", &c);
+        }
+        if ((c == ' ')||(c == EOF)) {
+            this->num.push_back(0);
+            free (buf);
+            return;
+        }
+        switch (c) {
+            case '1': buf[i] = 1;
+                ++i;
+                break;
+            case '2': buf[i] = 2;
+                ++i;
+                break;
+            case '3': buf[i] = 3;
+                ++i;
+                break;
+            case '4': buf[i] = 4;
+                ++i;
+                break;
+            case '5': buf[i] = 5;
+                ++i;
+                break;
+            case '6': buf[i] = 6;
+                ++i;
+                break;
+            case '7': buf[i] = 7;
+                ++i;
+                break;
+            case '8': buf[i] = 8;
+                ++i;
+                break;
+            case '9': buf[i] = 9;
+                ++i;
+                break;
+            }
+        bool flag = true;
+        while (flag){
+            fscanf(fp,"%c", &c);
+            switch (c) {
+            case '1': buf[i] = 1;
+                ++i;
+                break;
+            case '2': buf[i] = 2;
+                ++i;
+                break;
+            case '3': buf[i] = 3;
+                ++i;
+                break;
+            case '4': buf[i] = 4;
+                ++i;
+                break;
+            case '5': buf[i] = 5;
+                ++i;
+                break;
+            case '6': buf[i] = 6;
+                ++i;
+                break;
+            case '7': buf[i] = 7;
+                ++i;
+                break;
+            case '8': buf[i] = 8;
+                ++i;
+                break;
+            case '9': buf[i] = 9;
+                ++i;
+                break;
+            case '0': buf[i] = 0;
+                ++i;
+                break;
+            case ' ': flag = false;
+                ++i;
+                break;
+            case EOF: flag = false;
+                ++i;
+                break;
+            case '\n': flag = false;
+                ++i;
+                break;
+            case '\t': flag = false;
+                ++i;
+                break;
+            }
+            if ( i == cap_buf) {
+                buf = (short*) realloc(buf, cap_buf * 2 * sizeof(short));
+            }
+        }
+        --i;//сейчас ровно столько, сколько цифр в числе
+        int length_of_tail = i % radix_size;
+        int x;
+        for (int j = i-1; j >= length_of_tail; j-= radix_size) {
+            x = 0;
+            for (int q = 0; q < radix_size; ++q){
+                x += (pow((float)X,  (int)q) * buf[j - q]);
+            }
+            this->num.push_back(x);
+        }
+        x = 0;
+        for (int j = 0; j < length_of_tail; ++j){    	
+            x += (pow((float)X, (int) (length_of_tail -j-1)) * buf[j]);
+        }
+            if (x != 0) {
+                this->num.push_back(x);
+
+            }
+        free(buf);
+        return;
+    }
+
 
     void BigNum::output() {
         if (this->sign == false) {
@@ -159,6 +282,27 @@ using namespace std;
         }
         return;
     }
+    
+    void BigNum::foutput() {
+        FILE * p = fopen ("OUTPUT.TXT", "w+");
+        int size_of_vector = this->num.size_of_vector();
+        for (int i = size_of_vector - 1; i >= 0; --i) {
+            int w = radix / X;
+            if (i != (size_of_vector - 1)) {
+                int x = this->num[i];
+                int z = (x / w);
+                while ((z == 0)&&(w!=1)) {
+                    w = w / X;
+                    z = x/w;
+                    fprintf(p,"%d", 0);
+                }
+            }
+               fprintf(p, "%d", this->num[i]);
+        }
+        fclose(p);
+        return;
+    }
+    
     BigNum BigNum::min_size (BigNum *a, BigNum *b) { // по модулю!!!!!!
         a->sign = true;
         b->sign = true;
@@ -280,7 +424,7 @@ using namespace std;
     }
 
 
-    BigNum BigNum::Karatsuba (BigNum a){
+  /*  BigNum BigNum::Karatsuba (BigNum a){
         BigNum min = min_size(this, &a);
         BigNum max = max_size(this, &a);
         BigNum A_0;
@@ -318,8 +462,12 @@ using namespace std;
         rad = rad.power(2);
         major = major * rad;
         return puisne + med + major;
-    }
+    }*/
 
+    BigNum BigNum::Karatsuba (BigNum a){
+        BigNum four(4);
+        return ((a + *this).power(2) - (a-*this).power(2)) / four;
+    }
 
 
     BigNum  BigNum::operator + (BigNum a) {
